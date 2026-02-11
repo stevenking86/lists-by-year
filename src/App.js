f2f54772226cf86dbda2fb5b56cfd600a24cd818
Steven Gerhardt-King <stevengerhardt-king@MacBookPro.lan>
2026-02-11 14:58:51 +0000

    fix: remove unused App imports causing CI lint failure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import './lists';
 import Lists from './lists';
 
 function App() {
   return (
     <div className="App">
-      <Lists /> 
+      <Lists />
     </div>
   );
 }
